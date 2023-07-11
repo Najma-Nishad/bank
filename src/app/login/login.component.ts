758784696d9e32be877cbb9f987cf73223c733f6
unknown <najunajmapm@gmail.com>
2023-07-11 10:56:28 +0530

    initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  data="Happy Banking With Us...."  //string interpolation
+  pdata="Account Number"              // propertybinding
+
+  login(){
+    alert("login clicked")
+  }
+}
+
