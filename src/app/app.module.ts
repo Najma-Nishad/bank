f9f6c42b88ebb9a041e3ceb094f948d8f3623dc3
unknown <najunajmapm@gmail.com>
2023-07-22 20:07:56 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FooterComponent } from './footer/footer.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
 import { AccountsComponent } from './accounts/accounts.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,9 @@ import { AccountsComponent } from './accounts/accounts.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,11 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  data="Happy Banking With Us...."  //string interpolation
+  // data="Happy Banking With Us...."  //string interpolation
   pdata="Account Number"              // propertybinding
 
-  login(){
-    alert("login clicked")
+  login(){                          //
+    alert("login clicked")          //event binding
   }
+  accnoChange(event:any){             //
+    console.log(event.target.value);  // $event binding
+    }                                 //
 }
 
